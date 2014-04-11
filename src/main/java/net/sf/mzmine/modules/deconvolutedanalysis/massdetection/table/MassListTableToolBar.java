e917a2532560d26ed3a84287003a9b376127432c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2014-04-11 15:24:50 -0700

    added updated binbase-bus version with fixed retention index correction

---
+++ src/main/java/net/sf/mzmine/modules/deconvolutedanalysis/massdetection/table/MassListTableToolBar.java
@@ -0,0 +1,26 @@
+package net.sf.mzmine.modules.deconvolutedanalysis.massdetection.table;
+
+import net.sf.mzmine.util.GUIUtils;
+
+import javax.swing.*;
+import java.awt.*;
+
+class MassListTableToolBar extends JToolBar {
+	private static final Icon propertiesIcon = new ImageIcon("icons/propertiesicon.png");
+	private static final Icon printIcon = new ImageIcon("icons/printicon.png");
+
+	MassListTableToolBar(MassListTableWindow masterFrame) {
+		super(JToolBar.VERTICAL);
+
+		setFloatable(false);
+		setMargin(new Insets(5, 5, 5, 5));
+		setBackground(Color.white);
+
+		GUIUtils.addButton(this, null, propertiesIcon, masterFrame,
+				"Export as CSV", "Set table properties");
+
+		addSeparator();
+
+		GUIUtils.addButton(this, null, printIcon, masterFrame, "Print", "Print");
+	}
+}
